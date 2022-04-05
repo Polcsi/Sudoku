ba8e410e02e9ed4c1d900dc01e1ec86a143e8a82
Polcsi <pollakbence12@gmail.com>
2022-04-05 19:57:34 +0200

    improved writing file

--- Sudoku/Actions.cs
+++ Sudoku/Actions.cs
@@ -15,13 +15,25 @@ namespace Sudoku
         {
             using(StreamWriter fs = new StreamWriter($"{name}.txt", false))
             {
+
+                string border = "+";
+                for (int i = 0; i < settings.Count / 3; ++i)
+                {
+                    border += "-----+";
+                }
                 for (int i = 0; i < settings.Table.Length; ++i)
                 {
+                    if (i == 0) { fs.WriteLine(border); }
                     fs.Write("|" + settings.Table[i]);
                     if ((i + 1) % settings.Count == 0)
                     {
                         fs.WriteLine("|");
                     }
+                    if(i == ((settings.Count * ((settings.Count / 3) - 1)) + (settings.Count - 1)) || i == ((settings.Count * ((settings.Count / 3) + ((settings.Count / 3) - 1))) + (settings.Count - 1)))
+                    {
+                        fs.WriteLine(border);
+                    }
+                    if (i == settings.Table.Length - 1) { fs.WriteLine(border); }
                 }
             }
         }
